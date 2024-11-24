2b1691591cef5ac73785addad7a9c183b20d959f
Khangal Jargalsaikhan <khangal.j@irbis.sg>
2024-11-24 14:20:34 +0800

    remap copilot chat mappings

--- config/nvim/lua/tools/copilot.lua
+++ config/nvim/lua/tools/copilot.lua
@@ -1,4 +1,55 @@
 require("CopilotChat").setup {
   debug = true, -- Enable debugging
+  -- default mappings
+  mappings = {
+    complete = {
+      insert = '<Tab>',
+    },
+    close = {
+      normal = 'q',
+      insert = '<C-c>',
+    },
+    reset = {
+      normal = 'gl',
+      insert = 'gl',
+    },
+    submit_prompt = {
+      normal = '<CR>',
+      insert = '<C-s>',
+    },
+    toggle_sticky = {
+      detail = 'Makes line under cursor sticky or deletes sticky line.',
+      normal = 'gr',
+    },
+    accept_diff = {
+      normal = 'go',
+      insert = 'go'
+    },
+    jump_to_diff = {
+      normal = 'gj',
+    },
+    quickfix_diffs = {
+      normal = 'gq',
+    },
+    yank_diff = {
+      normal = 'gy',
+      register = '"',
+    },
+    show_diff = {
+      normal = 'gd',
+    },
+    show_system_prompt = {
+      normal = 'gp',
+    },
+    show_user_selection = {
+      normal = 'gs',
+    },
+    show_user_context = {
+      normal = 'gc',
+    },
+    show_help = {
+      normal = 'gh',
+    },
+  },
   -- See Configuration section for rest
 }
